113177fd279a8601478f88724678dd9665ac2452
Guilhem Niot <guilhem@gniot.fr>
2022-03-23 12:19:28 +0100

    Bump liboqs

--- test/src/main.rs
+++ test/src/main.rs
@@ -8,8 +8,19 @@ fn main() -> Result<()> {
     let start = Instant::now();
     kemalg.init()?;
     let elapsed = start.elapsed();
-    println!("Millis: {} ms", elapsed.as_millis());
+    println!("Init Millis: {} ms", elapsed.as_millis());
 
+    let start = Instant::now();
+    for _ in  1..10 {
+        let (kem_pk, kem_sk) = kemalg.keypair_async()?;
+        let (kem_ct, b_kem_ss) = kemalg.encapsulate(&kem_pk)?;
+        let a_kem_ss = kemalg.decapsulate(&kem_sk, &kem_ct)?;
+        assert_eq!(a_kem_ss, b_kem_ss);
+    }
+    let elapsed = start.elapsed();
+    println!("Normal Encapsulate millis: {} ms", elapsed.as_millis());
+
+    // A -> B: kem_pk
     let start = Instant::now();
     for _ in  1..10 {
         let (kem_pk, kem_sk) = kemalg.keypair_async()?;
@@ -18,7 +29,7 @@ fn main() -> Result<()> {
         assert_eq!(a_kem_ss, b_kem_ss);
     }
     let elapsed = start.elapsed();
-    println!("Millis: {} ms", elapsed.as_millis());
+    println!("Async Encapsulate millis: {} ms", elapsed.as_millis());
 
     oqs::sike_deinit();
 
